1ce4a64624a1e76ee3ce70a6556971c474705ce5
Roosevelt Villela <rmvillela@outlook.com>
2018-11-10 15:35:24 -0200

    CriaçCriação do frontend em ReactJS da aplicação

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import twitterLogin from '../twitter.svg';
+import './Login.css';
+
+export default class Login extends Component {
+  state = {
+    username: ""
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem('@GoTwitter:username', username);
+
+    this.props.history.push('/timeline');
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="login-wrapper">
+        <img src={twitterLogin} alt="GoTwitter"/>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.username}
+            onChange={this.handleInputChange} 
+            type="text" 
+            placeholder="Nome de usuário" 
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
+  }
+}
